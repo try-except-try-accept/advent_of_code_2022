e7eae1ee21da2b5e71de7d43ed9ed7d1f0e9f2b8
try-except-try-accept <chall168421@gmail.com>
2022-12-05 12:18:04 +0700

    day 5 - stacks

---
+++ day5b.py
@@ -0,0 +1,89 @@
+from re import search, match, findall
+from collections import Counter
+from helpers import PuzzleHelper
+
+PP_ARGS = False, False #rotate, cast int
+
+DAY = 5
+TEST_DELIM = "---"
+FILE_DELIM = "\n"
+TESTS = """    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2///MCD
+"""
+
+DEBUG = True
+
+
+def solve(data):
+    count = 0
+
+    for i, line in enumerate(data):
+
+
+        if line.replace(" ", "").isdigit():
+
+            num_stacks = int(line.strip()[-1])
+            sep = i
+            break
+
+    stacks = [[] for i in range(num_stacks)]
+
+    
+
+    for line in data[:sep]:
+        for i in range(1, num_stacks*4, 4):
+
+            if line[i] != " ":
+
+                crate = line[i]
+                stacks[i//4].insert(0, crate)
+
+
+    print(stacks)
+
+    input()
+
+        
+            
+    for line in data[sep+2:]:
+        line = line.split()
+
+        amt = int(line[1])
+        from_stack = int(line[3])
+        to_stack = int(line[5])
+
+        move_crates = []
+        for i in range(amt):
+            move_crates.append(stacks[from_stack-1].pop())
+
+        for m in reversed(move_crates):
+            stacks[to_stack-1].append(m)
+
+
+    msg = ""
+    for i in stacks:
+        msg += i.pop(-1)
+        
+
+        
+    
+
+    return msg
+
+
+
+
+if __name__ == "__main__":
+    p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
+
+    if p.check(TESTS, solve):
+        puzzle_input = p.load_puzzle()
+        puzzle_input = p.pre_process(puzzle_input, *PP_ARGS)
+        print("FINAL ANSWER: ", solve(puzzle_input))
