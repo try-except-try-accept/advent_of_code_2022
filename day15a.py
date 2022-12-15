899f7aec8da184d6ca109f39adda989aea8bc10f
try-except-try-accept <chall168421@gmail.com>
2022-12-15 20:37:35 +0700

    day 15 - part a complete - beacons/sensors/coords

--- day15a.py
+++ day15a.py
@@ -22,7 +22,7 @@ Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
 Sensor at x=20, y=1: closest beacon is at x=15, y=3///26"""
 
-DEBUG = True
+DEBUG = False
 
 row_min, row_max = 0, 0
 
@@ -31,137 +31,75 @@ def set_row_min_max(x, y):
     global row_min, row_max
 
     if x < row_min:
-
         row_min = x
 
     elif x > row_max:
         row_max = x
 
-
-def compute_points(a, b):
-
-    #print("Computing points between", a, "and", b)
-
-    x1, y1 = a
-    x2, y2 = b
-    x_diff = abs(x2 - x1)
-    y_diff = abs(y2 - y1)
-    x_inc = 1 if x1 < x2 else -1
-    y_inc = 1 if y1 < y2 else -1
-
-    if abs(x_diff) > abs(y_diff):
-        #print("more x to change")
-
-        x_inc = x_diff / y_diff
-
-    else:
-        #print("more y to change")
-        y_inc = y_diff / x_diff
-
-    #print("will change x by", x_inc, "and y by", y_inc)
-
-    x_dec = False
-    x_dec = x1 > x2
-
-    y_dec = False
-    y_dec = y1 > y2
-    
-    pts = set()
-
-    done = False
-    while not done:
-        pts.add((int(x1), int(y1)))
-        
-        if x_dec:
-            x1 -= x_inc
-        else:
-            x1 += x_inc
-
-        if y_dec:
-            y1 -= y_inc
-        else:
-            y1 += y_inc
-        
-
-        if x_dec and x1 < x2 or not x_dec and x1 > x2 or y_dec and y1 < y2 or not y_dec and y1 > y2:
-            
-            return pts
-
-    
-
-
        
 
-
-def display(poly, sensor, beacon):
-
-    
-
+def display(polys, beacons, sensors, impossible):
+    if not DEBUG:   return False
     s = ""
-    for y in range(-50, 100+1):
-        for x in range(-50, 100+1):
+    p.bugprint(f"displaying {len(polys)} polys")
+    p.bugprint("".join(chr(i) for i in range(38, 98)))
+    for y in range(-10, 50):
+        s += str(y).zfill(3) + " "
+        for x in range(-10, 50+1):
             c = "."
-            if (x, y) in poly:
-                c = "#"
-
-            elif (x, y) == (sensor.x, sensor.y):
+            if x in impossible and y == Y_WANTED:
+                c = "X"
+            for pol in polys:
+                if (x, y) == pol.left: c = "L"
+                if (x, y) == pol.top: c = "U"
+                if (x, y) == pol.bott: c = "D"
+                if (x, y) == pol.right: c = "R"
+                
+            if any([(x, y) == (s.x, s.y) for s in sensors]):
                 c = "S"
-
-            elif (x, y) == (beacon.x, beacon.y):
+            if any([(x, y) == (b.x, b.y) for b in beacons]):
                 c = "B"
-
             s += c
         s += "\n"
-
-    print(s)
-
-            
+    p.bugprint(s)
 
 def manhattan(x1, y1, x2, y2):
-
-
     return abs(x1-x2) + abs(y1-y2)
+
+class Polygon:
+
+    def __init__(self, x, y, left, top, right, bott, dim):
+        self.x = x
+        self.y = y
+        self.left = left
+        self.top = top
+        self.right = right
+        self.bott = bott
+        self.dim = dim
     
 
 class Sensor:
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, polys):
         set_row_min_max(x, y)
-        
         self.x = x
         self.y = y
         self.beacon = None
         self.impossible = None
-
-        
-
+        self.polys = polys
 
     def get_impossible_beacons(self, beacon):
         self.beacon = beacon
-
         dist = manhattan(self.x, self.y, beacon.x, beacon.y)
-        
-
-        print("Beacon is at", beacon.x, ",", beacon.y)
-
-        print("Therefore no beacons within this range:")
-
-        self.left = (self.x-dist, self.y)
-        self.top = (self.x, self.y-dist)
-        self.right =(self.x+dist, self.y)
-        self.bott = (self.x, self.y+dist)
-
-        self.polygon = [self.left, self.top, self.right, self.bott]
-
-        self.dim = dist * 2
-
-        display(self.polygon, self, beacon)
-
-        
-        
-
-        
-
+        p.bugprint("Beacon is at", beacon.x, ",", beacon.y)
+        p.bugprint("Therefore no beacons within this range:")
+        self.polys.append(Polygon(self.x, self.y,
+                                 (self.x-dist, self.y),
+                                 (self.x, self.y-dist),
+                                 (self.x+dist, self.y),
+                                 (self.x, self.y+dist),
+                                  dist * 2))
+ 
 
 class Beacon:
     def __init__(self, x, y):
@@ -175,54 +113,52 @@ def solve(data):
 
     sensors = {}
     beacons = {}
+    polys = []
     current_sensor = None
     impossible = set()
 
     for row in data:
-        print(row)
+        p.bugprint(row)
 
         row = row.split()
 
         sx, sy, bx, by = row[2], row[3][:-1], row[8], row[9]
-        print(sx, sy, bx, by)
+        p.bugprint(sx, sy, bx, by)
 
         sensor_code = sx+sy
 
-        new_sensor = eval(f"Sensor({sx} {sy})")
-
+        new_sensor = eval(f"Sensor({sx} {sy}, polys=polys)")
         sensors[sensor_code] = new_sensor
-
         current_sensor = new_sensor
-        
         beacon_code = bx+by
         beacon = beacons.get(beacon_code)
         if beacon is None:
             beacon = eval(f"Beacon({bx} {by})")
+            beacons[beacon_code] = beacon
         current_sensor.get_impossible_beacons(beacon)
         
 
-
-        
     print("Processed sensors/beacons")
-
     all_bounds = []
 
     
-    polys_affected = [sensor.polygon for sensor in sensors if sensor.top > Y_WANTED and sensor.bott > Y_WANTED]
+    polys_affected = [poly for poly in polys if poly.top[1] < Y_WANTED and poly.bott[1] > Y_WANTED]
 
+    impossible = set()
     for poly in polys_affected:
-        row = poly.dim - poly.y - Y_WANTED
-        print(row)
- 
-    print("impossible", impossible)
-    print("possible", all_possible, "entire row", len(all_possible), row_min, "to", row_max)
+        x_adjust = abs((poly.dim // 2) - abs(poly.y - Y_WANTED))
+        p.bugprint("x adjust is", x_adjust)
+        impossible |= set(range(poly.x - x_adjust, poly.x + x_adjust + 1))
 
+    print("Computed impossible coords")
+
+    for obj in list(beacons.values()) + list(sensors.values()):
+        if obj.y == Y_WANTED:
+            impossible.remove(obj.x)
+
+    display(polys_affected, beacons.values(), sensors.values(), impossible)
+    
     return len(impossible)
-
-
-
-compute_points((-100, 0), (-110, 50))
-
 
 if __name__ == "__main__":
     p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
