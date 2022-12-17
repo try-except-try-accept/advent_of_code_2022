375a10be2aabb59554560514d3c5b97aada4915d
try-except-try-accept <chall168421@gmail.com>
2022-12-17 12:12:35 +0700

    day 16 - too hard!

--- day16a.py
+++ day16a.py
@@ -1,6 +1,11 @@
 from re import search, match, findall
 from collections import Counter
 from helpers import PuzzleHelper
+from math import inf as INF
+try:
+    from dijkstar import Graph, find_path
+except:
+    print("dijkstar not installed")
 
 
 PP_ARGS = False, False #rotate, cast int
@@ -20,123 +25,83 @@ Valve II has flow rate=0; tunnels lead to valves AA, JJ
 Valve JJ has flow rate=21; tunnel leads to valve II///1651"""
 
 DEBUG = True
-
-
-def calc_valve_distance(v1, v2, d=0, found=False, visited=None):
-
-    return len(a_star_search(graph, v1, v2).nodes)
-
    
 
 
-def a_star_search(goal, start):
-    global table
-
-    print("find way to", start, "from", goal)
-    
-    goal_node = v_map[goal]
-    start_node = v_map[start]
-
-
-    # get all distances    
-    distances = [calculate_distance(n, goal_node) for n in v_map.values()]
-
-    max_distance = max(distances)
-
+def dijkstra(start, goal):
+    # Dijkstra's Algorithm
+    origin = start
     # set up table
-                                
-    table = {n:{"node":str(n),
-                "g-cost":INF if n is not start_node else 0,
-                "h-cost":calculate_distance(n, goal_node),
-                "f-cost":INF,
-                "prev":None, "calc":""} for n in v_map.values()}   
+    table = {n:{"node":str(n), "cost":INF if n is not origin else 0, "prev":"", "calc":""} for n in v_map.values()}                        
+    unvisited = list(v_map.values())
+    visited = []
 
-    # OPEN - the set of nodes to be evaluated
-    # add the start node to open
-    open_nodes = set([start_node])
-    # CLOSED - the set of nodes already evaluated
-    closed_nodes = set()
-    current_node = None
+    print("From", start, "to", goal)
 
-    while len(open_nodes):
-            #p.bugprint("open nodes looping")
+    while len(unvisited):
 
-            ## find the open list node with smallest known f-cost                    
+            smallest = INF
+            current_node = None
 
-            current_node = min(open_nodes, key=lambda n: table[n]["f-cost"])
+            ## find the unvisited vertex with smallest known distance from the origin
+            for node, data in table.items():
+                    if node not in unvisited:        continue
+                    if data["cost"] < smallest:
+                            smallest = data["cost"]
+                            current_node = node
+            p.bugprint("The current vertex is", current_node)
 
-            if current_node == goal_node:
-                break
+            ## find each unvisited neighbour of the current vertex
 
-            ## remove current from OPEN
-            open_nodes.remove(current_node)
+            for neighbour in current_node.connections:
+                    
+                    p.bugprint(f"{current_node} is connected to {neighbour}")
+                    if neighbour not in unvisited:
+                            p.bugprint("Already visited")
+                    else:
+                            ## calculate total distance from origin node
+                            tot = neighbour.weight
+                            calc = str(tot)
+                            # until we've got back to the start
+                            p.bugprint(f"We need to make it back from {current_node} to {origin}")
+                            pitstop = table[current_node]["cost"]
+                            p.bugprint(f"Came via {current_node} and that cost {pitstop}")
+                            tot += pitstop
+                            calc += " + " + str(pitstop)
+                            p.bugprint(f"so the total is now {tot}")
 
-            closed_nodes.add(current_node)
+                            if tot < table[neighbour]["cost"]:
+                                    table[neighbour]["prev"] = current_node                                                        
+                                    table[neighbour]["cost"] = tot                                                                                                              
+                                    table[neighbour]["calc"] = calc
+                                    
+                                    
+                                    for node, data in table.items():
+                                            p.bugprint(
+                                                    f'{str(node)} \t\t {data["cost"]} \t\t {data["prev"]} \t\t {data["calc"]}')
 
-            ## find each un-closed neighbour of the current node
-
-            #p.bugprint(f" i have {len(current_node.neighbours)} neighbours")
-            for neighbour in current_node.neighbours:
-
-                    if neighbour in closed_nodes:
-                        continue
-             
-
-                    g_cost = 1
-                    calc = str(g_cost)        
-                    pitstop = table[current_node]["g-cost"]
-
-                    g_cost += pitstop
-                                                            
-                    calc += " + " + str(pitstop)                             
-                    h_cost = table[neighbour]["h-cost"]                                        
-                    calc += f" + {h_cost}"
-
-                    f_cost = g_cost + h_cost
-
-                    if neighbour not in open_nodes or g_cost < table[neighbour]["g-cost"]:
-                            open_nodes.add(neighbour)
-                            table[neighbour]["prev"] = current_node                                                        
-                            table[neighbour]["f-cost"] = f_cost
-                            table[neighbour]["g-cost"] = g_cost                                                                                
-                            table[neighbour]["calc"] = calc
-
-                    open_nodes.add(neighbour)
-
-    destination = goal_node
-    node = destination
-    path = [destination]
+                                    p.bugprint()
+                            else:
+                                    p.bugprint(f"{tot} was not shorter. table not updated")
+            unvisited.remove(current_node)
 
 
 
-    while node is not start_node:
 
-        
-        #p.bugprint("im at", node)
-        try:
+            node = goal
+
+            
+    path = [str(goal)]
+
+    while node is not origin:
+
             node = table[node]["prev"]
-        except KeyError:
-            return INF # some paths are impossible!
-        path.append(node)
+            path.append(str(node))
 
-    path = list(reversed(path))
-    path_copy = list(path)
-
-    next_node = path_copy.pop(0)
-
-    s = ""
-
-    while path_copy:
-        next_node = path_copy.pop()
-        direction_map[next_node.y][next_node.x] = next_node.elevation.upper()
-
-    length = len(path) + 1 - ERROR_CORRECT
-
-        
-    return length
+    return path
 
 
-
+  
 
 v_map = None
     
@@ -148,15 +113,36 @@ class Valve:
         self.connections = []
         self.flow = 0
         self.open = False
+        self.weight = 1
 
     def get_eventual_pressure(self, current, min_remain):      
-        distance = a_star_search(current, self)
+        distance = dijkstra(current, self)
         return ((min_remain-distance) * self.flow)
 
     def __repr__(self):
         return self.label
 
 v_map = {}
+
+def get_next_best_valve(current, minutes_left, best_map, d=0):
+
+
+    for n in current.connections:
+        if n in best_map:   continue
+
+        best_map[n] = {"valve":n,
+                       "pressure_potential":0 if n.open else n.flow * (minutes_left - d),
+                      "distance":d}
+
+        get_next_best_valve(n, minutes_left-1, best_map, d+1)
+
+        
+
+        
+
+    
+
+    
 
 
 def get_or_make_valve(label, v_map):
@@ -165,83 +151,112 @@ def get_or_make_valve(label, v_map):
     if valve is None:
         valve = Valve(label)
         v_map[label] = valve
-        print("Added", label)
+        p.bugprint("Added", label)
 
     return valve
-    
-    
-    
+
+
+g = None
+
+
+ 
+
+def solve_2(data):
+    global g
+    g = Graph()
+
+    # set up nodes
+
+    node_data = {}
+
+    for line in data:
+        line = line.replace("valves", "valve")        
+        this_valve = line[6:8].strip()     
+        flow = int(search("\d{1,3}", line).group())
+        path_weight = 1/flow if flow else 1
+
+        node_data[this_valve] = {"flow":flow,
+                                 "weight":path_weight,
+                                 "connections":[]}
+
+        
+        for connected_valve in line.split("to valve ")[1].split(", "):
+            node_data[this_valve]["connections"].append(connected_valve)
+
+
+
+    for this_valve, data in node_data.items():
+
+        for connected_valve in data["connections"]:
+            weight = node_data[connected_valve]["weight"]
+            g.add_edge(this_valve, connected_valve, (weight))
+            print(f"I connected {this_valve} to {connected_valve}")
+
+        
+        
+
+    current = "AA"
+
+
+
+    all_valves = list(g)
+    for m in range(30, -1, -1):
+        # find highest pressure potential = next node
+
+        for n in all_valves:
+            if n == current:    continue
+
+            path = find_path(g, current, n, cost_func=get_dist).nodes
+            print(f"It takes {len(path)-1} steps to get to {n} path: {path}")
+            
+        paths = [(n, node_data[n]["flow"]*(m-(len(find_path(g, current, n).nodes)-1))) for n in all_valves]
+
+        for p in paths:
+            print(p)
+        
+        # find best path to next node
+
+        # go to next node, turn on pitstop node IF node in top half of wanted nodes?
+        print("Best valves to go to are", options)
+        
 
 def solve(data):
     global v_map
     count = 0
 
     minute = 1
+    goal = None
 
     for line in data:
-
-        line = line.replace("valves", "valve")
-        
-        this_valve = get_or_make_valve(line[6:8].strip(), v_map)
-        
+        line = line.replace("valves", "valve")        
+        this_valve = get_or_make_valve(line[6:8].strip(), v_map)        
         flow = int(search("\d{1,3}", line).group())
         this_valve.flow = flow
+        this_valve.weight = 1/flow if flow else 1
 
+        if goal is None or this_valve.flow > goal.flow:
+            goal = this_valve
         for v in line.split("to valve ")[1].split(","):           
             connected_valve = get_or_make_valve(v.strip(), v_map)
-            this_valve.connections.append(connected_valve)
-            
-            
+            this_valve.connections.append(connected_valve)            
 
+    start = v_map["AA"]
+    start.open = None
+
+
+    for m in range(30):
+
+        # find node with best pressure potential
+
+        node = get_best_pressure_potential
+
+        # this is J
+
+        # find longest path to J
+
+        # turn on interim nodes if node in top half of wanted
 
         
-            
-
-    current = v_map["AA"]
-    current.open = None
-
-    open_valves = []
-
-    mins_required = 0
-
-    
-    for m in range(30, -1, -1):
-
-        
-            
-        print(f"== Minute {30-m} ==")
-
-        if mins_required:
-            print("Was spent moving")
-            mins_required -= 1
-            continue
-            
-
-        # find the best eventual flow
-        
-
-        best = max([v for v in v_map.values() if v.open == False], key=lambda v: v.get_eventual_pressure(current, m))
-
-        mins_required = distance = a_star_search(current, best)
-        
-        print("takes", mins_required, "to move from", current, "to", best)
-
-        current = best
-
-        current.open = True
-
-
-
-        pressure = sum(v.flow for v in v_map.values() if v.open)
-
-        print(f"Valves {','.join(v.label for v in v_map.values() if v.open)} are open, releasing {pressure} pressure.")
-    
-    
-
-    return count
-
-
-
 
 if __name__ == "__main__":
     p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
