88c66277929d78cc53384fc1cf576c2238f011dd
try-except-try-accept <chall168421@gmail.com>
2022-12-29 16:15:54 +0700

    day 17 part a - bitwise tetris FINALLY!

--- day17a.py
+++ day17a.py
@@ -1,6 +1,7 @@
 from re import search, match, findall
 from collections import Counter
 from helpers import PuzzleHelper
+from math import ceil
 
 import tetris
 
@@ -9,157 +10,165 @@ PP_ARGS = False, False #rotate, cast int
 DAY = 17
 TEST_DELIM = "---"
 FILE_DELIM = "\n"
-TESTS = """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>///3068"""
+TESTS = """<///4448
+--->>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>///3068"""
 MOVE_LIMIT = 2022
 
 WIDTH = 7
 DEBUG = False
 
-##block_q = [[0b011110],                                      # flat line
-##           [0b0001000, 0b0011100, 0b0001000],               # cross
-##           [0b0000100, 0b0000100, 0b0011100],               # L shape
-##           [0b0010000, 0b0010000, 0b0010000, 0b0010000],    # vert line
-##           [0b0011000, 0b0011000]]                          # square
+def init_block_q():
+                  ##0123456789ABCDEFGHIJKLMNOPQR
+    return [(1,0b011110),                          # flat line
+           (3, 0b000100000111000001000),           # cross
+           (3, 0b000010000001000011100),           # L shape
+           (4, 0b0010000001000000100000010000),    # vert line
+           (2, 0b00110000011000)]                  # square
 
-            ##0123456789ABCDEFGHIJKLMNOPQR
-block_q = [0b011110,                          # flat line
-           0b000100000111000001000,           # cross
-           0b000010000001000011100,           # L shape
-           0b0010000001000000100000010000,    # vert line
-           0b00110000011000]                  # square
-
+block_q = []
 
 MASK = 0b1111111
 
 LEFT_MASK =  0b1000000100000010000001000000
 RIGHT_MASK = 0b0000001000000100000010000001
 
-def isolate_lines(block=0, line=0, num_lines=1):
+def isolate_lines(block=0, line=0, num_lines=1, block_height=0):
     """Extract a single line from a block"""
-    rows = get_block_length(block)
 
-
-    shift = rows - (line + num_lines)
-    #p.bugprint("Shift is", shift, "bc", rows, "rows and", line, "line")
-
-    length = 7 * 5
-
-    #p.bugprint(f"Block is {bin(block): >70} ".center(length))
+    shift = max(0, block_height - (line + num_lines))
     block = block >> shift * WIDTH
 
     mask = ((MASK+1) ** num_lines) - 1
-
-    #p.bugprint(f"Mask is  {bin(mask): >70}".center(length))
 
     return block & mask
 
   
 
-def get_block_length(block):
-    """Determine how many rows a binary block takes up"""
-    poss_length = 0
-    while True:
-        if block < 2 ** (WIDTH * poss_length):
-            #p.bugprint("This block has length", poss_length)
-            return poss_length
-        poss_length += 1
-
-def determine_bounds(block=0, fall_point=0, rested=0):
+def determine_bounds(block=0, fall_point=0, rested=0, block_height=0, num_rows_rested=0):
 
     """Determines the current bounds based on already rested blocks
 and the fall point of the current block"""
-    block_length = get_block_length(block)
+
     if fall_point <= 0:
         return None
     else:
         # get the rested from the fall point up to the fall point + the length of the block
         line_start = 0 # isolate lines from 0, unless fall point has exceeded top line
-        if fall_point > block_length:
-            line_start =  fall_point - block_length
-        return isolate_lines(rested, line_start, min(fall_point, block_length))
+        if fall_point > block_height:
+            line_start =  fall_point - block_height
+        return isolate_lines(rested, line_start, min(fall_point, block_height), num_rows_rested)
 
+def display_interim(block):
+    if DEBUG:
+        debinarise(block)        
 
+def check_if_cant_move(fall_point, bounds, block):
+    left = block << 1 & bounds == 0
+    right = block >> 1 & bounds == 0
+    return not (left or right)
+    
 def process_jet_stream(bounds=0, block=0, jet=None):
     """Accepts a current block and one or more jet instructions, and
     bounds according to rested blocks and fall point"""
     jet = list(jet)
-    p.bugprint("Got bounds its", bounds)
-    p.bugprint("Got block its", block)
-    
 
     while jet:
+        didnt_move = False
 
         this_jet = jet.pop(0)
         # if already on edge, just skip
         if this_jet == "<":
             p.bugprint("Jet of gas pushes rock left", end="")
             if block & LEFT_MASK != 0:
-                p.bugprint(", but nothing happens.")
+                p.bugprint(", but nothing happens as hit left wall")
+                display_interim(block)
+                didnt_move = True
                 continue
         elif this_jet == ">":
             p.bugprint("Jet of gas pushes rock right", end="")
             if block & RIGHT_MASK != 0:
-                p.bugprint(", but nothing happens.")
+                p.bugprint(", but nothing happens as hit right wall")
+                display_interim(block)
+                didnt_move = True
                 continue
         p.bugprint()
+        
 
         
         old_block = block
         #p.bugprint("Block was", bin(block))
         block = eval(f"{block} {this_jet*2} 1")
+        
         #p.bugprint("Block now", bin(block))
         if bounds:
                 
             # check against landed blocks
             if block & bounds != 0:
-                #p.bugprint(", but nothing happens.")
+                p.bugprint(", but nothing happens as hit rested")
                 block = old_block
+                didnt_move = True
+        display_interim(block)
 
-    return block
+    return block, didnt_move
 
     
-def rest_blocks(rested=0, block=0, fall_point=0):
+def rest_blocks(rested=0, block=0, fall_point=0, num_rows_rested=0, block_height=0):
     """Amalgamate already rested blocks with newly resting block
 at a given fall point"""
-    rows_rested = get_block_length(rested)
 
-    if rows_rested == 0:
-        return block
 
-    shift_needed = rows_rested - fall_point
+    if num_rows_rested == 0:
+        return block, block_height, block_height
+    shift_needed = num_rows_rested - fall_point
+    
 
     block = block << (WIDTH * shift_needed)
 
     if rested & block != 0:
         raise Exception("merge error - trying to merge overlapping blocks")
 
-    shift = 0
 
-## this is cruddy
-##    for i in range(rows_rested-1): 
-##        this_line = isolate_lines(rested, i, 1)
-##        next_line = isolate_lines(rested, i+1, 1)
-##        if this_line | next_line == MASK:
-##            print("Found truncation point...")
-##            shift = i
-##            break
+    # cannot decrease
 
-    return (rested | block) >> (WIDTH * shift)
+    newly_rested = max(0, (block_height - fall_point))
+    
+    num_now_rested = num_rows_rested +  newly_rested
+    
+    rested = (rested | block) # >> (WIDTH * shift_needed)   # why need to shift back?
+
+    truncate = 0
 
 
-def check_if_cant_fall(fall_point=0, rested=0, block=0):
+    for line in range(fall_point - block_height, fall_point + 1 + newly_rested):
+
+        if isolate_lines(block=rested, line=line, num_lines=1, block_height=num_now_rested) ^ MASK == 0: # full row found
+
+            truncate = num_now_rested - line
+            break
+
+
+    if truncate:
+
+        rested = rested >> (WIDTH * truncate)
+        num_now_rested = (num_rows_rested - truncate) + newly_rested
+
+
+
+
+    return rested, num_now_rested, newly_rested
+
+def check_if_cant_fall(fall_point=0, rested=0, block=0, num_rows_rested=0):
     """Return true if rested blocks below prevent current block from falling further"""
     # shift block to correlate with correct row of rested
     
     # if AND result is not 0, return True
 
-    rows_rested = get_block_length(rested)
-
-    if fall_point == rows_rested:
+    if fall_point == num_rows_rested:
         return True
 
 
-    shift_req = rows_rested - fall_point - 1
+    shift_req = max(0, (num_rows_rested - fall_point)-1)
+
 
     block = block << (WIDTH * shift_req)
 
@@ -169,89 +178,132 @@ def check_if_cant_fall(fall_point=0, rested=0, block=0):
     return False
     
     
+def get_jets(num, jet_q):
+
+    for i in range(num):
+        jet = jet_q.pop(0)        
+        jet_q.append(jet)
+        yield jet
+
 
 
 def solve(data):
-
+    global DEBUG
   
 
-    
+    block_q = init_block_q()
 
     jet_q = list(data[0])
 
     rested = 0
 
     bounds = 0
-  
+    num_rows_rested = 0
+    overall_rested = 0
+
+    
 
 
     for turn in range(MOVE_LIMIT):
-        print("turn", turn)
+
+  
+
+        p.bugprint("A new block begins to fall")
+        bounds = 0
 
         # get next block
         block = block_q.pop(0)
         block_q.append(block)
+        block_height, block = block
         
         # can always fall at least 3 spaces
-        next_3_jets = jet_q[:3]
-        jet_q += list(next_3_jets)
 
+
+        jet = get_jets(3, jet_q)
+        
         fall_point = 0
 
-        block = process_jet_stream(bounds, block, next_3_jets)
 
-        couldnt_fall = check_if_cant_fall(fall_point, rested, block)
+        block, _ = process_jet_stream(bounds, block, jet)
+
+        couldnt_fall = check_if_cant_fall(fall_point, rested, block, num_rows_rested)
 
         
         landed = False
         while not landed:
-
-            jet = jet_q[:3]
-            jet_q += [jet]
-
-
-            block = process_jet_stream(bounds, block, jet)
             
-            cant_fall = check_if_cant_fall(fall_point, rested, block)
+            jet = get_jets(1, jet_q)
+            
+            block, didnt_move = process_jet_stream(bounds, block, jet)
+            bounds = determine_bounds(block, fall_point, rested, block_height, num_rows_rested)
 
-            if not cant_fall:       # can fall
+            #debinarise(block)
+            #debinarise(rested)
+            p.bugprint("Rock falls 1 unit")
+
+            
+            cant_fall = check_if_cant_fall(fall_point, rested, block, num_rows_rested)
+            if not cant_fall:
                 fall_point += 1
-                bounds = determine_bounds(block, fall_point, rested)
-            elif couldnt_fall:      # can't fall, and couldn't fall last time
+               
+                bounds = determine_bounds(block, fall_point, rested, block_height, num_rows_rested)
+
+            if cant_fall:
+
                 p.bugprint("Block came to rest")
                 landed = True
                 
-                rested = rest_blocks(fall_point=fall_point, rested=rested, block=block)
+                rested, num_rows_rested, newly_rested = rest_blocks(fall_point=fall_point, rested=rested,
+                                                      block=block, num_rows_rested=num_rows_rested,
+                                                      block_height=block_height)
 
-            couldnt_fall = cant_fall
+                overall_rested += newly_rested
 
-            
-    return get_block_length(rested)      
+                #debinarise(rested)
+
+    return overall_rested
         
 def debinarise(num):
+    if type(num) == tuple:  num = num[0]
     try:
-
+        
         num = bin(num)[2:]
+        p.bugprint(len(num))
+        fill_row = WIDTH * ceil(len(num) / WIDTH)
+        p.bugprint("Will z fill", fill_row)
+        num = num.zfill(fill_row)
 
         for i in range(0, len(num), WIDTH):
-            p.bugprint(num[i:i+WIDTH+1].replace("0", ".").replace("1", "█"))
+            p.bugprint(num[i:i+WIDTH].replace("0", ".").replace("1", "█"))
         
-    except:
+    except Exception as e:
+        p.bugprint(e)
         p.bugprint(f"Problem debinarising {num}")
-        
-    
-def binarise(data):
+
+
+def convert_arg(data):
     if set(data) == set(["█", "."]):
-
         data = data.replace(".", "0").replace("█", "1")
-        return int(data, 2)
-
+        data = int(data, 2)
 
     try:
-        return int(data)
+        return int(data)                              # return integer value
     except ValueError:
-        if data in "TrueFalseNone": return eval(data)
-        return data
+        if data in "TrueFalseNone": return eval(data) # return bool/null flag
+        return data                                   # return > / < string
+    
+def process_test_args(data):
+
+    data = tuple(map(convert_arg, data.split(",")))
+
+    if len(data) == 1:  data = data[0]
+
+    return data
+
+    
+        
+    
+   
                
 def module_tests():
     with open("day17.tests", encoding="utf-8") as f:
@@ -265,15 +317,16 @@ def module_tests():
     p.bugprint(f"processing {test_data.count('test:')}")
     for line in test_data.splitlines():
 
-        
 
         if "test:" in line:
             p.bugprint(line)
             e = None
             if func:
                 
-                data = {arg:binarise(value) for arg, value in data.items()}
+                data = {arg:process_test_args(value) for arg, value in data.items()}
                 expected = data.pop("result")
+
+                p.bugprint(data)
 
                 actual = eval(f"{func}(**data)")
      
@@ -283,7 +336,7 @@ def module_tests():
                     p.bugprint(f"received")
                     debinarise(actual)
                     raise Exception(f"Test number {test_num} failed\n{e}")
-                p.bugprint(f"Test number {test_num} PASSED 🎉")
+                p.bugprint(f"Test number {test_num} ({func}) PASSED 🎉")
                 test_num += 1
 
                 p.bugprint()
@@ -326,7 +379,9 @@ def module_tests():
 if __name__ == "__main__":
     
     p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
-    module_tests()
+    if input("Run module tests?"):
+        module_tests()
+        input("all module tests passed")
 
     if p.check(TESTS, solve):
         puzzle_input = p.load_puzzle()
