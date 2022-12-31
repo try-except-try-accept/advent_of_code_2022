f5d178ad9b8dbb5078d0c3924d1a9648d1b38bce
unknown <chall168421@gmail.com>
2022-12-31 16:41:29 +0700

    day 22 part b - 3D cube traversal - lambda and numpy rotate help

--- day22b.py
+++ day22b.py
@@ -1,6 +1,7 @@
 from re import search, match, findall
 from collections import Counter
 from helpers import PuzzleHelper
+from numpy import rot90
 
 PP_ARGS = False, False #rotate, cast int
 
@@ -22,16 +23,9 @@ TESTS = """        ...#
 
 10R5L5R10L4R5L5///5031"""
 
-TESTS = """    ..
-    ..
-......
-......
-    ....
-    ....
 
-1L10R///1000"""
 
-DEBUG = True
+DEBUG = False
 
 
 RDLU = ((1, 0),
@@ -39,17 +33,67 @@ RDLU = ((1, 0),
         (-1, 0),
         (0, -1))
         
+RIGHT = 0
+DOWN = 1
+LEFT = 2
+UP = 3
 
+top, back, left, front, bottom, right = 0, 1, 2, 3, 4, 5
+
+FACE_MATRIX = [row.split("\t") for row in '''right	left	front	right	right	top
+front	bottom	bottom	bottom	back	back
+left	right	back	left	left	bottom
+back	top	top	top	front	front'''.splitlines()]
+
+
+ORIENTATION_MATRIX = [[eval(i) for i in row.split("\t")] for row in '''LEFT	RIGHT	RIGHT	DOWN	RIGHT	LEFT
+DOWN	UP	RIGHT	DOWN	UP	RIGHT
+DOWN	UP	LEFT	LEFT	UP	LEFT
+DOWN	DOWN	RIGHT	UP	UP	LEFT'''.splitlines()]
+
+invert = lambda n, dim : abs(n-(dim-1))
+
+x_and_invert_y =     lambda x, y, dim: (x, invert(y, dim))
+x_and_zero =         lambda x, y, dim: (x, 0)
+y_and_zero =         lambda x, y, dim: (y, 0)
+invert_x_and_zero =  lambda x, y, dim: (invert(x, dim), 0)
+invert_y_and_zero =  lambda x, y, dim: (invert(y, dim), 0)
+
+invert_x_and_y =     lambda x, y, dim: (invert(x, dim), y)
+invert_y_and_max =   lambda x, y, dim: (invert(y, dim), dim - 1)
+
+zero_and_y =         lambda x, y, dim: (0, y)
+zero_and_x =         lambda x, y, dim: (0, x)
+zero_and_invert_x =  lambda x, y, dim: (0, invert(x, dim))
+
+
+max_and_y =          lambda x, y, dim: (dim - 1, y)
+max_and_invert_x =   lambda x, y, dim: (dim - 1, invert(x, dim))
+max_and_invert_y =   lambda x, y, dim: (dim - 1, invert(y, dim))
+x_and_max =          lambda x, y, dim: (x, dim - 1)
+
+
+
+                    #top	        back	            left	        front	            bottom	            right
+LOCATION_MATRIX = ((max_and_invert_y,	zero_and_y,	    zero_and_y,	        invert_y_and_zero,  zero_and_y,	            x_and_invert_y),            # right
+                    (x_and_zero,	invert_x_and_y,	    zero_and_invert_x,	x_and_zero,	    invert_x_and_y,	    zero_and_invert_x),         # down
+                    (y_and_zero,	invert_y_and_max,   max_and_y,	        max_and_y,	    invert_y_and_max,       max_and_y),                 # left
+                    (invert_x_and_y,	invert_x_and_y,	    zero_and_x,	        x_and_max,	    x_and_max,	            max_and_invert_x))          # up
+
+
+def rotate(array, rotations=3):
+    for _ in range(rotations):
+        array = rot90(array)
+
+    return [list(row) for row in array]
+    
 
 def move(x, y, dim, num, orientation, current_face, faces):
 
-    print(faces)
-
-    input()
-    last_actual_x, last_actual_y = x, y
-
     
 
+    
+    last_face = current_face
     last_actual_x, last_actual_y = x, y
     dir_symbol = ">V<^"[orientation]
     
@@ -63,14 +107,16 @@ def move(x, y, dim, num, orientation, current_face, faces):
     p.bugprint(f"Move {dir_symbol} for {num} steps")
 
     #display(faces, dim)
-
+    last_orientation = orientation
     
 
     while num > 0:
         
         
+        p.bugprint("last face was", last_face)
+        
         vector = RDLU[orientation]
-        print("was At", nx, ny)
+        p.bugprint("was At", nx, ny)
         p.bugprint(f"Orientation is {'>V<^'[orientation]}, current face is {current_face}, vector is {vector}")
         
         
@@ -78,86 +124,50 @@ def move(x, y, dim, num, orientation, current_face, faces):
         nx = nx + vector[0]
         ny = ny + vector[1]
 
-        print("now At", nx, ny)
+        ax, ay = None, None
 
-        new_orientation = None
+        if nx < 0:
+            ax, ay = 0, ny
+        elif ny < 0:
+            ax, ay = nx, 0
+        elif nx == dim:
+            ax, ay = dim-1, ny
+        elif ny == dim:
+            ax, ay = nx, dim - 1
+
+
+
+        if ax is not None or ay is not None:
+            fnum = eval(current_face)
+        
+            current_face = FACE_MATRIX[last_orientation][fnum]
+
+            orientation = ORIENTATION_MATRIX[last_orientation][fnum]
+
+            p.bugprint("Location matrix index at", last_orientation, fnum)
+            nx, ny = LOCATION_MATRIX[last_orientation][fnum](ax, ay, dim)
 
         
         
-        if ny < 0:              # off top of face
-            new_orientation = {"left":0, "back":1, "right":2, "top":1}.get(current_face)
-            current_face = {"top":"back", "bottom":"front", "front":"top", "left":"top", "back":"bottom", "right":"top"}[current_face]
-            
-        elif ny > dim-1:
-            p.bugprint(" off bottom of face")
-            new_orientation = {"left":0, "bottom":3, "right":0}.get(current_face)
-            current_face = {"top":"front", "bottom":"back", "front":"bottom", "left":"bottom", "back":"bottom", "right":"bottom"}[current_face]          
-
-        elif nx < 0:            # off left of face
-            new_orientation = {"top":1, "bottom":3, "back":3}.get(current_face)
-            current_face = {"top":"left", "bottom":"left", "front":"left", "left":"back", "back":"right", "right":"bottom"}[current_face]
-
-        elif nx > dim-1:          # off right of face
-            p.bugprint("Went off right")
-            new_orientation = {"front":1, "top":2, "right":2}.get(current_face)
-            current_face = {"top":"right", "bottom":"right", "front":"right", "left":"front", "back":"left", "right":"top"}[current_face]
-
-        if new_orientation is None: new_orientation = orientation
-        orientation_change = new_orientation - orientation
-
-        if orientation_change == 0:
-            ny %= dim
-            nx %= dim
-
-        elif orientation_change == 1:
-            print("orientation change 1", new_orientation-orientation)
-
-            nx = (dim-1) - ny
-            ny = dim-1            
-            orientation = new_orientation
-
-        elif orientation_change == 3:
-            print("orientation change 3", orientation_change)
-            ny = nx
-            orientation = new_orientation
-            nx = dim-1
-        elif orientation_change == -3:
-            print("orientation change 3", orientation_change)
-            ny = 0
-            orientation = new_orientation
-            
-
-        elif orientation_change == 2:
-            print("orientation change 2", orientation_change)
-            nx = (dim-1) - nx
-            ny = ny - 1     
-            orientation = new_orientation
-
-        elif orientation_change == -2:
-            print("orientation change 2", orientation_change)
-            nx = (dim-1) - nx
-            ny = ny + 1
-                
-            orientation = new_orientation
-
-        print("Now at", nx, ny)
-       
 
         if faces[current_face][ny][nx] == "#":         # if hit brick, last pos was final pos
+            p.bugprint("Hit brick on", current_face, "at", nx, ny)
             return last_actual_x, last_actual_y, last_orientation, last_face
 
-        else:                                # else hit free space ., counts as a move
-            
+        else:            
+            p.bugprint("No obstacle  on", current_face, "at", nx, ny)
             last_actual_x, last_actual_y = nx, ny
             dir_symbol = ">V<^"[orientation]
             faces[current_face][ny][nx] = dir_symbol
             
-            display(faces, dim)
+            if DEBUG:
+                display(faces, dim)
+                
             num -= 1
             p.bugprint(num, "steps remain..")
             last_face = current_face
             last_orientation = orientation
-            
+
             
         
     return last_actual_x, last_actual_y, orientation, current_face
@@ -167,18 +177,18 @@ def move(x, y, dim, num, orientation, current_face, faces):
 def display(faces, dim):
 
     for row in faces["top"]:
-        p.bugprint((dim*2*" ") + "".join(row))
+        print((dim*2*" ") + "".join(row))
 
 
     for one, two, three in zip(faces["back"], faces["left"], faces["front"]):
-        p.bugprint("".join(one+two+three))
+        print("".join(one+two+three))
     
                 
     for one, two in zip(faces["bottom"], faces["right"]):
         
-        p.bugprint((dim*2*" ") + "".join(one+two))
+        print((dim*2*" ") + "".join(one+two))
     
-    #input()
+    p.buginput()
         
     
 
@@ -189,28 +199,71 @@ def solve(data):
 
     
     board_map = data[:-2]
+
+
+
+    
     board_map = [list(row.strip()) for row in board_map]
+
+    
+        
     
     path = list(data[-1])
 
 
+
+
+    if TRANSLATE:
+        # puzzle data
+        DIM = len(board_map) // 4
+        
+        line1 = board_map[:DIM]
+        line2 = board_map[DIM:DIM*2]
+        line3 = board_map[DIM*2:DIM*3]
+        line4 = board_map[DIM*3:DIM*4]
+
+
+
+        for line in (line1, line2, line3, line4):
+            
+            print(len(line), len(line[0]))
+
+        faces = {
+
+        'top' : [row[:DIM] for row in line1],
+        'back' : rotate([row[:DIM] for row in line4]),
+        'left' : rotate([row[:DIM] for row in line3]),
+        'front' : [row[:DIM] for row in line2],
+        'bottom' : [row[DIM:(DIM*2)] for row in line3],
+        'right' : rotate([row[(DIM):DIM*2] for row in line1], 2)
+
+        }
+
+
+    else:
+        # test data cube net
+        DIM = len(board_map) // 3
+
+        
+
+        middle_faces = board_map[DIM:(DIM*2)]
+
+        faces = {
+
+        'top' : [row for row in board_map[:DIM]],
+        'back' : [row[:DIM] for row in middle_faces],
+        'left' : [row[DIM:(DIM*2)] for row in middle_faces],
+        'front' : [row[(DIM*2):(DIM*3)] for row in middle_faces],
+        'bottom' : [row[:DIM] for row in board_map[(DIM*2):]],
+        'right' : [row[DIM:(DIM*2)] for row in board_map[(DIM*2):]]
+
+        }
+
+    for f, data in faces.items():
+
+        print(f, len(data), len(data[0]))
     
-    DIM = len(board_map) // 3
 
-    middle_faces = board_map[DIM:(DIM*2)]
-
-    faces = {
-
-    'top' : [row for row in board_map[:DIM]],
-    'back' : [row[:DIM] for row in middle_faces],
-    'left' : [row[DIM:(DIM*2)] for row in middle_faces],
-    'front' : [row[(DIM*2):(DIM*3)] for row in middle_faces],
-    'bottom' : [row[:DIM] for row in board_map[(DIM*2):]],
-    'right' : [row[DIM:(DIM*2)] for row in board_map[(DIM*2):]]
-
-    }
-
-    p.bugprint(faces["bottom"]); input()
     
 
     orientation = 0
@@ -230,8 +283,12 @@ def solve(data):
         next_char = path.pop(0)
 
         if next_char in "LR":
-
-            x, y, orientation, current_face = move(x, y, DIM, int(num), orientation, current_face, faces)
+            try:
+                x, y, orientation, current_face = move(x, y, DIM, int(num), orientation, current_face, faces)
+            except Exception as e:
+                display(faces, DIM)
+                raise e
+                
             num = ""
             
             if next_char == "L":
@@ -249,12 +306,24 @@ def solve(data):
         num = ""
 
 
-    face_adjust = {"top":(DIM*2, 0),
+    print("Final x y", x, y, "current face", current_face)
+
+    if not TRANSLATE:
+        # test data
+        face_adjust = {"top":(DIM*2, 0),
                    "back":(0, DIM),
                    "left":(DIM, DIM),
                    "front":(DIM*2, DIM),
                    "bottom":(DIM*2, DIM*2),
                    "right":(DIM*3, DIM*2)}[current_face]
+    else:
+        # puzzle input
+        face_adjust = {"top":(DIM, 0),
+                   "back":(0, DIM*3),
+                   "left":(0, DIM*2),
+                   "front":(DIM, DIM),
+                   "bottom":(DIM, DIM*2),
+                   "right":(DIM*3, 0)}[current_face]
 
     p.bugprint("Face adjust", face_adjust)
 
@@ -263,17 +332,19 @@ def solve(data):
 
     print("Final row", final_row)
     print("Final col", final_col)
+    print("Final facing", orientation)
     
-    
+    display(faces, DIM)
     return sum([1000 * final_row, 4 * final_col, orientation])
 
-
+TRANSLATE = False
 
 
 if __name__ == "__main__":
     p = PuzzleHelper(DAY, TEST_DELIM, FILE_DELIM, DEBUG, PP_ARGS)
     p.module_tests(locals())
     if p.check(TESTS, solve):
+        TRANSLATE = True
         puzzle_input = p.load_puzzle()
         puzzle_input = p.pre_process(puzzle_input, *PP_ARGS)
         print("FINAL ANSWER: ", solve(puzzle_input))
